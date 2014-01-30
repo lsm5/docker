0e48d19178daa801ac698f24eaaead17652be72d
Alexander Larsson <alexl@redhat.com>
2014-01-30 23:20:10 +0100

    Make execdriver a per container property
    This way we can have different (non-default) exec drivers for some
    containers.
    
    Docker-DCO-1.1-Signed-off-by: Alexander Larsson <alexl@redhat.com> (github: alexlarsson)

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -174,6 +174,7 @@ func (daemon *Daemon) register(container *Container, updateSuffixarray bool) err
 		return err
 	}
 
+	container.execDriver = daemon.execDriver
 	container.daemon = daemon
 
 	// Attach to stdout and stderr
@@ -1010,14 +1011,6 @@ func (daemon *Daemon) Diff(container *Container) (archive.Archive, error) {
 		daemon.driver.Put(container.ID)
 		return err
 	}), nil
-}
-
-func (daemon *Daemon) Run(c *Container, pipes *execdriver.Pipes, startCallback execdriver.StartCallback) (int, error) {
-	return daemon.execDriver.Run(c.command, pipes, startCallback)
-}
-
-func (daemon *Daemon) Kill(c *Container, sig int) error {
-	return daemon.execDriver.Kill(c.command, sig)
 }
 
 // Nuke kills all containers then removes all content
