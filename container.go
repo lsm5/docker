252f3163f24b8437bdb34fe9c018c753afa38c8e
Alexander Larsson <alexl@redhat.com>
2014-01-30 23:20:10 +0100

    Make execdriver a per container property
    This way we can have different (non-default) exec drivers for some
    containers.
    
    Docker-DCO-1.1-Signed-off-by: Alexander Larsson <alexl@redhat.com> (github: alexlarsson)

--- container.go
+++ container.go
@@ -37,6 +37,8 @@ type Container struct {
 	root   string // Path to the "home" of the container, including metadata.
 	basefs string // Path to the graphdriver mountpoint
 
+	execDriver execdriver.Driver
+
 	ID string
 
 	Created time.Time
@@ -778,8 +780,15 @@ func (container *Container) monitor(callback execdriver.StartCallback) error {
 		exitCode int
 	)
 
-	pipes := execdriver.NewPipes(container.stdin, container.stdout, container.stderr, container.Config.OpenStdin)
-	exitCode, err = container.runtime.Run(container, pipes, callback)
+	if container.command == nil {
+		// This happends when you have a GHOST container with lxc
+		populateCommand(container)
+		err = container.execDriver.Restore(container.command)
+	} else {
+		pipes := execdriver.NewPipes(container.stdin, container.stdout, container.stderr, container.Config.OpenStdin)
+		exitCode, err = container.execDriver.Run(container.command, pipes, callback)
+	}
+
 	if err != nil {
 		utils.Errorf("Error running container: %s", err)
 	}
@@ -854,7 +863,7 @@ func (container *Container) kill(sig int) error {
 	if !container.State.IsRunning() {
 		return nil
 	}
-	return container.runtime.Kill(container, sig)
+	return container.execDriver.Kill(container.command, sig)
 }
 
 func (container *Container) Kill() error {
@@ -873,7 +882,7 @@ func (container *Container) Kill() error {
 			return fmt.Errorf("lxc-kill failed, impossible to kill the container %s", utils.TruncateID(container.ID))
 		}
 		log.Printf("Container %s failed to exit within 10 seconds of lxc-kill %s - trying direct SIGKILL", "SIGKILL", utils.TruncateID(container.ID))
-		if err := container.runtime.Kill(container, 9); err != nil {
+		if err := container.execDriver.Kill(container.command, 9); err != nil {
 			return err
 		}
 	}
