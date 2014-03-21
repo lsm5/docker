757b9d49ea7673789f5c90486bfaf6c0a2be9f0d
Alexander Larsson <alexl@redhat.com>
2014-03-21 11:53:40 +0100

    Add "foreground" mode for docker run and docker start
    This is a mode of "docker run" that uses direct forking from the
    docker client, meaning that the container is a direct child of the
    docker run command (rather than the daemon), and that it directly
    inherits stdout/err (and stdin if requested) from the parent of the
    "docker run" command.
    
    This is very useful in cases where you e.g. want to run a docker
    container from systemd (or other process manager), as we can use the
    process manager to e.g. set up cgroups and limits, monitor and
    restart, pass in fds for socket activation, etc.
    
    The way this works is that there is a new execdriver, which is used
    for forground containers. This container just does rpc calls back to
    the "docker run" command, which in turn uses a "real" execdriver and
    feeds it whatever data the "fake" execdriver got, but in the context
    of the docker run command.
    
    We feed information about the process (exit status, pid, etc) to the
    daemon, so it is visible just like any other docker container via the
    docker apis.
    
    Docker-DCO-1.1-Signed-off-by: Alexander Larsson <alexl@redhat.com> (github: alexlarsson)
    
    Conflicts:
    	daemon/container.go
    
    	modified:   api/client/commands.go
    	modified:   daemon/container.go
    	modified:   daemon/daemon.go
    	new file:   daemon/execdriver/foreground/cmd.go
    	new file:   daemon/execdriver/foreground/driver.go
    	new file:   daemon/execdriver/foreground/refcount.go
    	modified:   runconfig/hostconfig.go
    	modified:   runconfig/parse.go
    	modified:   server/server.go
    
    Conflicts:
    	daemon/container.go

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -16,6 +16,7 @@ import (
 	"github.com/dotcloud/docker/archive"
 	"github.com/dotcloud/docker/daemon/execdriver"
 	"github.com/dotcloud/docker/daemon/execdriver/execdrivers"
+	"github.com/dotcloud/docker/daemon/execdriver/foreground"
 	"github.com/dotcloud/docker/daemon/execdriver/lxc"
 	"github.com/dotcloud/docker/daemon/graphdriver"
 	_ "github.com/dotcloud/docker/daemon/graphdriver/vfs"
@@ -176,6 +177,10 @@ func (daemon *Daemon) register(container *Container, updateSuffixarray bool) err
 
 	container.execDriver = daemon.execDriver
 	container.daemon = daemon
+
+	if container.hostConfig.CliAddress != "" {
+		container.execDriver = foreground.NewDriver(container.hostConfig.CliAddress, daemon.config.Root, daemon.sysInitPath, daemon.execDriver)
+	}
 
 	// Attach to stdout and stderr
 	container.stderr = utils.NewWriteBroadcaster()
