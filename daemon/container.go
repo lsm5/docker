515028c99f9a6537308a4e7f0c0ede5487e995c7
Alexander Larsson <alexl@redhat.com>
2014-03-21 11:53:40 +0100

    Add "foreground" mode for docker run and docker start
    This is a mode of "docker run" that uses direct forking from the
    docker client, meaning that the container is a direct child of the
    docker run command (rather than the daemon), and that it directly
    inherits stdout/err (and stdin if requested) from the parent of the
    "docker run" command.
    
    This is very useful in cases where you e.g. want to run a docker
    container from systemd (or other process manager), as we can use the
    process manager to e.g. set up cgroups and limits, monitor and
    restart, pass in fds for socket activation, etc.
    
    The way this works is that there is a new execdriver, which is used
    for forground containers. This container just does rpc calls back to
    the "docker run" command, which in turn uses a "real" execdriver and
    feeds it whatever data the "fake" execdriver got, but in the context
    of the docker run command.
    
    We feed information about the process (exit status, pid, etc) to the
    daemon, so it is visible just like any other docker container via the
    docker apis.
    
    Docker-DCO-1.1-Signed-off-by: Alexander Larsson <alexl@redhat.com> (github: alexlarsson)
    
    Conflicts:
    	daemon/container.go
    
    	modified:   api/client/commands.go
    	modified:   daemon/container.go
    	modified:   daemon/daemon.go
    	new file:   daemon/execdriver/foreground/cmd.go
    	new file:   daemon/execdriver/foreground/driver.go
    	new file:   daemon/execdriver/foreground/refcount.go
    	modified:   runconfig/hostconfig.go
    	modified:   runconfig/parse.go
    	modified:   server/server.go
    
    Conflicts:
    	daemon/container.go

--- daemon/container.go
+++ daemon/container.go
@@ -4,6 +4,17 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/dotcloud/docker/archive"
+	"github.com/dotcloud/docker/daemon/execdriver"
+	"github.com/dotcloud/docker/daemon/execdriver/foreground"
+	"github.com/dotcloud/docker/daemon/graphdriver"
+	"github.com/dotcloud/docker/engine"
+	"github.com/dotcloud/docker/image"
+	"github.com/dotcloud/docker/links"
+	"github.com/dotcloud/docker/nat"
+	"github.com/dotcloud/docker/runconfig"
+	"github.com/dotcloud/docker/utils"
+	"github.com/dotcloud/docker/pkg/label"
 	"io"
 	"io/ioutil"
 	"log"
@@ -15,19 +26,9 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/dotcloud/docker/archive"
-	"github.com/dotcloud/docker/daemon/execdriver"
-	"github.com/dotcloud/docker/daemon/graphdriver"
-	"github.com/dotcloud/docker/engine"
-	"github.com/dotcloud/docker/image"
-	"github.com/dotcloud/docker/links"
-	"github.com/dotcloud/docker/nat"
-	"github.com/dotcloud/docker/pkg/label"
 	"github.com/dotcloud/docker/pkg/networkfs/etchosts"
 	"github.com/dotcloud/docker/pkg/networkfs/resolvconf"
 	"github.com/dotcloud/docker/pkg/symlink"
-	"github.com/dotcloud/docker/runconfig"
-	"github.com/dotcloud/docker/utils"
 )
 
 const DefaultPathEnv = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
@@ -296,6 +297,10 @@ func (container *Container) Start() (err error) {
 		return err
 	}
 	container.waitLock = make(chan struct{})
+
+	if container.hostConfig.CliAddress != "" {
+		container.execDriver = foreground.NewDriver(container.hostConfig.CliAddress, container.daemon.config.Root, container.daemon.sysInitPath, container.daemon.execDriver)
+	}
 
 	return container.waitForStart()
 }
