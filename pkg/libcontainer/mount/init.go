98b01f3fcdbcee7a9430c9fbd2e89123347e55b6
Dan Walsh <dwalsh@redhat.com>
2014-05-21 15:31:59 -0400

    Mount /run with correct SELinux label
    Docker-DCO-1.1-Signed-off-by: Daniel Walsh <dwalsh@redhat.com> (github: rhatdan)
    
    Conflicts:
    	pkg/libcontainer/mount/init.go

--- pkg/libcontainer/mount/init.go
+++ pkg/libcontainer/mount/init.go
@@ -222,6 +222,7 @@ func newSystemMounts(rootfs, mountLabel string, mounts libcontainer.Mounts) []mo
 		{source: "tmpfs", path: filepath.Join(rootfs, "dev"), device: "tmpfs", flags: syscall.MS_NOSUID | syscall.MS_STRICTATIME, data: label.FormatMountLabel("mode=755", mountLabel)},
 		{source: "shm", path: filepath.Join(rootfs, "dev", "shm"), device: "tmpfs", flags: defaultMountFlags, data: label.FormatMountLabel("mode=1777,size=65536k", mountLabel)},
 		{source: "devpts", path: filepath.Join(rootfs, "dev", "pts"), device: "devpts", flags: syscall.MS_NOSUID | syscall.MS_NOEXEC, data: label.FormatMountLabel("newinstance,ptmxmode=0666,mode=620,gid=5", mountLabel)},
+		{source: "tmpfs", path: filepath.Join(rootfs, "run"), device: "tmpfs", flags: defaultMountFlags, data: label.FormatMountLabel("", mountLabel)},
 	}
 
 	return systemMounts
